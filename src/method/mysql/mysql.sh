901ff51f273b0f5164f2619da4c6f4b7db58b8f8
izgit <ugo.viti@initzero.it>
2023-02-14 09:32:26 +0100

    removed the '-C' (compression) option from mysql method because was deprecated since MySQL 8.0.32

--- src/method/mysql/mysql.sh
+++ src/method/mysql/mysql.sh
@@ -28,7 +28,7 @@ config_field_import backup_source_uri uri  notnull	"${usr_file_conf}" keep
 
 # variables useful to this method
 backup_method_cmd="mysqldump"
-backup_method_opts_default="-C --force --databases --opt --quote-names --events --routines --triggers --hex-blob"
+backup_method_opts_default="--force --databases --opt --quote-names --events --routines --triggers --hex-blob"
 #backup_method_opts=""
 
 backup_name_prefix="backup"
